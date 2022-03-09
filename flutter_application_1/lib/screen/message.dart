11f7044d0d390f35ddbc4620d5607d4e75d91023
Bhajuram Chaurasiya <cram6138@gmail.com>
2022-03-09 15:37:51 +0530

    Chat related changes

--- flutter_application_1/lib/screen/message.dart
+++ flutter_application_1/lib/screen/message.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/model/chatMsg.dart';
 
 class Message extends StatefulWidget {
   @override
@@ -6,16 +7,142 @@ class Message extends StatefulWidget {
 }
 
 class _MessageState extends State<Message> {
+  List<ChatMessage> messages = [
+    ChatMessage(messageContent: "Hello, Will", messageType: "receiver"),
+    ChatMessage(messageContent: "How have you been?", messageType: "receiver"),
+    ChatMessage(
+        messageContent: "Hey Kriss, I am doing fine dude. wbu?",
+        messageType: "sender"),
+    ChatMessage(messageContent: "ehhhh, doing OK.", messageType: "receiver"),
+    ChatMessage(
+        messageContent: "Is there any thing wrong?", messageType: "sender"),
+  ];
+
+  String chatMessageVal = "";
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("Message"),
-        backgroundColor: const Color.fromARGB(255, 15, 68, 112),
-      ),
-      body: const Center(
-        child: Text("Message Page"),
-      ),
-    );
+        appBar: AppBar(
+          title: const Text("Message"),
+          elevation: 0,
+          automaticallyImplyLeading: false,
+          backgroundColor: Colors.white,
+          flexibleSpace: SafeArea(
+            child: Container(
+              padding: const EdgeInsets.only(right: 16.0),
+              child: Row(
+                children: [
+                  IconButton(
+                      onPressed: () {
+                        Navigator.pop(context);
+                      },
+                      icon: const Icon(
+                        Icons.arrow_back,
+                        color: Colors.black,
+                      )),
+                  const SizedBox(
+                    width: 2,
+                  ),
+                  const CircleAvatar(
+                    backgroundImage: NetworkImage(
+                        "https://randomuser.me/api/portraits/men/5.jpg"),
+                    maxRadius: 20,
+                  ),
+                  const SizedBox(
+                    width: 12,
+                  ),
+                  Expanded(
+                      child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      const Text(
+                        "Kriss Benwat",
+                        style: TextStyle(
+                            fontSize: 16, fontWeight: FontWeight.w600),
+                      ),
+                      const SizedBox(
+                        height: 6,
+                      ),
+                      Text(
+                        "Online",
+                        style: TextStyle(color: Colors.grey[600], fontSize: 13),
+                      ),
+                    ],
+                  ))
+                ],
+              ),
+            ),
+          ),
+        ),
+        body: SingleChildScrollView(
+          child: Container(
+            margin: const EdgeInsets.fromLTRB(10, 0, 10, 30.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: [
+                ListView.builder(
+                  itemCount: messages.length,
+                  shrinkWrap: true,
+                  padding: const EdgeInsets.only(top: 10, bottom: 10),
+                  physics: const NeverScrollableScrollPhysics(),
+                  itemBuilder: (context, index) {
+                    return Container(
+                      padding: const EdgeInsets.only(
+                          left: 16, right: 16, top: 10, bottom: 10),
+                      child: Text(messages[index].messageContent),
+                    );
+                  },
+                ),
+                Card(
+                  color: const Color.fromARGB(255, 125, 239, 243),
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20.0)),
+                  child: const Padding(
+                    padding:
+                        EdgeInsets.all(15), //apply padding to all four sides
+                    child: Text("Hello World, Text 1"),
+                  ),
+                ),
+                Row(
+                  children: [
+                    Expanded(
+                      child: TextField(
+                        decoration: const InputDecoration(
+                          icon: Icon(
+                            Icons.add,
+                            size: 20,
+                            color: Colors.blue,
+                          ),
+                        ),
+                        onChanged: (val) {
+                          setState(() {
+                            chatMessageVal = val;
+                          });
+                        },
+                      ),
+                    ),
+                    FloatingActionButton(
+                      onPressed: () {
+                        setState(() {
+                          messages.add(ChatMessage(
+                              messageContent: chatMessageVal,
+                              messageType: "Sender"));
+                        });
+                        print(chatMessageVal);
+                      },
+                      child: const Icon(
+                        Icons.send,
+                        size: 18,
+                      ),
+                      elevation: 0,
+                    )
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ));
   }
 }
